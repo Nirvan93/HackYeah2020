f70d527679bc525d1961e27b8dbe9e6a56d640aa
Molioo <molik94@gmail.com>
2020-11-27 19:42:56 +0100

    Added few helper scripts, added TCameraController first version, will be improved

---
+++ HackYeah/HackYeah/Assets/Code/THelpers/ScreenshotHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenshotHelper : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode _keyCodeForScreenshot = KeyCode.Q;
+
+    private int _index = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(_keyCodeForScreenshot))
+        {
+            ScreenCapture.CaptureScreenshot("D://Screenshot" + _index + ".png");
+            _index += 1;
+        }
+    }
+
+    
+}
---
+++ HackYeah/HackYeah/Assets/Code/Tests/CameraTestHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTestHelper : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 10;
+
+    private Rigidbody _rigidbody = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        _rigidbody.velocity = Vector3.right * horizontal * _speed;
+    }
+}
