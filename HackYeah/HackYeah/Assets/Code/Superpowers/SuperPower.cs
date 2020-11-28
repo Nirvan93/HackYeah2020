0557e8c0953bec1081098145e30605ffed4c0d5b
Molioo <molik94@gmail.com>
2020-11-28 09:39:06 +0100

    Added superpowers manager and superpowers

---
+++ HackYeah/HackYeah/Assets/Code/Superpowers/SuperPower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SerializeField]
+public class SuperPower : MonoBehaviour
+{
+    public ESuperPowerType SuperPowerType;
+
+    public KeyCode KeyCodeToActivate = KeyCode.Alpha0;
+
+    public bool IsAvailable = false;
+
+    public virtual void OnActivate()
+    {
+        Debug.Log("Activated superpower " + SuperPowerType.ToString());
+    }
+
+    public virtual void OnDeactivate()
+    {
+        Debug.Log("Dectivated superpower" + SuperPowerType.ToString());
+    }
+}
---
+++ HackYeah/HackYeah/Assets/Code/Superpowers/SuperpowersManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperpowersManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<SuperPower> _allSuperPowers = new List<SuperPower>();
+
+    private SuperPower _activeSuperPower = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckChangeSuperPowerInput();
+    }
+
+    public void SetSuperPowerAvailable(ESuperPowerType superPowerType)
+    {
+        foreach (SuperPower superPower in _allSuperPowers)
+        {
+            if (superPower.SuperPowerType == superPowerType)
+                superPower.IsAvailable = true;
+        }
+    }
+
+    private void CheckChangeSuperPowerInput()
+    {
+        foreach(SuperPower superPower in _allSuperPowers)
+        {
+            if(superPower.IsAvailable && Input.GetKeyDown(superPower.KeyCodeToActivate))
+            {
+                SetCurrentSuperPower(superPower);
+                break;
+            }
+        }
+    }
+
+    private void SetCurrentSuperPower(SuperPower superPowerToActivate)
+    {
+        if (_activeSuperPower != null)
+            _activeSuperPower.OnDeactivate();
+
+        _activeSuperPower = superPowerToActivate;
+        _activeSuperPower.OnActivate();
+    }
+}
