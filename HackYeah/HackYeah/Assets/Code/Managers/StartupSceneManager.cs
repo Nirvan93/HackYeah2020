25aff912e0fe4f3c75b91618757080696d5376bc
Molioo <molik94@gmail.com>
2020-11-28 12:16:29 +0100

    Added TStartupScene, Main Menu, Levels Manager

---
+++ HackYeah/HackYeah/Assets/Code/Managers/StartupSceneManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupSceneManager : MonoBehaviour
+{
+    public void Start()
+    {
+        LevelsManager.Instance.LoadScene("MainMenu");
+    }
+}
