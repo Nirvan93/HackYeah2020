f3ebec18ec6919b67136bbd90f1c4faf4c1d8777
Molioo <molik94@gmail.com>
2020-11-28 12:48:04 +0100

    Added game ui with ui super powers icons

---
+++ HackYeah/HackYeah/Assets/Code/UI/UiSuperPower.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiSuperPower : MonoBehaviour
+{
+    public ESuperPowerType SuperPowerType = ESuperPowerType.SuperStrength;
+
+    [SerializeField]
+    private float _notActivatedSize = 100;
+    [SerializeField]
+    private float _activatedSize = 150;
+
+    [SerializeField]
+    private RectTransform _powerRect;
+
+
+    public void SetActivatedValue(bool activated)
+    {
+        _powerRect.sizeDelta = activated ? new Vector2(_activatedSize, _activatedSize) : new Vector2(_notActivatedSize, _notActivatedSize);
+    }
+}
