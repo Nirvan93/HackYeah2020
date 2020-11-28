0fec2f8abcc1d2573d0564a71fb3a284b9229e61
ewalkowka <ewalkowka98@gmail.com>
2020-11-29 00:34:43 +0100

    audio

---
+++ HackYeah/HackYeah/Assets/Code/Enemy/EnemyAlienAudio.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAlienAudio : MonoBehaviour
+{
+    public bool IsPlayerOnTrigger = false;
+    void Update()
+    {
+        if()
+    }
+}
---
+++ HackYeah/HackYeah/Assets/SoundTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    EnemyAlienAudio audio;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            audio.IsPlayerOnTrigger = true;
+    }
+}
