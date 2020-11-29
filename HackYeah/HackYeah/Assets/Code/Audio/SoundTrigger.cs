1dbb97ceb974b5b36baa7c47d7d8f45fb230e6b7
ewalkowka <ewalkowka98@gmail.com>
2020-11-29 01:09:45 +0100

    audio alien

---
+++ HackYeah/HackYeah/Assets/Code/Audio/SoundTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+    public GameObject PlayAudioPosition;
+    EnemyAlienAudio audio;
+    public List<AudioClip> AudioList;
+            
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        AudioController.GetClip(AudioList);
+
+        int selected = Random.Range(0, AudioList.Count);
+
+        AudioController.PlayAudio(AudioList[selected], PlayAudioPosition.transform.position);
+    }
+}
--- HackYeah/HackYeah/Assets/Code/Enemy/EnemyAlienAudio.cs
+++ HackYeah/HackYeah/Assets/Code/Enemy/EnemyAlienAudio.cs
@@ -1,12 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyAlienAudio : MonoBehaviour
 {
-    public bool IsPlayerOnTrigger = false;
-    void Update()
-    {
-        if()
-    }
+   
 }
